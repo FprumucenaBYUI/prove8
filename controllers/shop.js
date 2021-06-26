68ca3e112c2d82b7ab100596e5b324bfa8b9eb0a
Flavio Prumucena <flaprumucena@byui.edu>
2021-06-26 13:23:38 -0600

    prove 9

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,10 +1,10 @@
 const https = require('https')
 
-const ITEMS_PER_PAGE = 10 // Limit of 10 items per page.
+const ITEMS_PER_PAGE = 9 // Limit of 10 items per page.
 
 const renderIndex = (req, res, json) => {
     let searchedValue = req.body.searchValue || req.query.searchValue || '' // Handle for GET, POST or neither
-    let page = req.query.page || 1 // Grab our page number, 1 if undefined
+    let page = +req.query.page || 1 // Grab our page number, 1 if undefined
 
     const indexStart = (page - 1) * ITEMS_PER_PAGE // Item index to start on...
     const indexEnd = page * ITEMS_PER_PAGE
@@ -13,13 +13,26 @@ const renderIndex = (req, res, json) => {
         x.name.toLowerCase().includes(searchedValue.toLowerCase())
     )
 
+    // let totalProducts = filteredData.length * 1;
+    // console.log(ITEMS_PER_PAGE * page);
+    // console.log("SEARCHED VALUE = [" + searchedValue + "]");
+    console.log("CURRENT PAGE = [" + page + "]");
+    console.log("LAST PAGE = [" + Math.ceil(filteredData.length / ITEMS_PER_PAGE) + "]");
+    let nextP = page + 1;
+    console.log("NEXT PAGE = [" + nextP + "]");
+
+
     res.render('shop/product-list', {
         data: filteredData.slice(indexStart, indexEnd), // For JSON/Array and not Mongoose, .slice() works best.
         path: '/',
-        title: 'Week 8 Prove Assignment',
-        searchedValue: searchedValue,
-        page: page,
-        numPages: Math.ceil(filteredData.length / ITEMS_PER_PAGE)
+        title: 'Week 9 Prove Assignment',
+        // searchedValue: searchedValue,
+        page: page, //current page
+        numPages: Math.ceil(filteredData.length / ITEMS_PER_PAGE), //lastPage
+        previousPage: page - 1,
+        nextPage: page + 1,
+        hasPreviousPage: page > 1,
+        hasNextPage: ITEMS_PER_PAGE * page < filteredData.length
     });
 }
 
